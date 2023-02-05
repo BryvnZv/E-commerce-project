68bf64c593898395eb916c29b50a322e2a5b2251
BryvnZv <bryvnzv@gmail.com>
2023-02-04 21:00:45 -0500

    created checkout page along with checkout items functionality

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
 import Shop from "./routes/shop/shop.component";
+import CheckOut from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<CheckOut />} />
       </Route>
     </Routes>
   );
